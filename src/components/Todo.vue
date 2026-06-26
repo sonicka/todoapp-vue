9b01db1d6489759d5ecce6b6f814e7e81a281b1a
Sona Leitmanova <sona.leitmanova@gmail.com>
2026-06-26 15:17:45 +0200

    fix hidden buttons

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -29,30 +29,28 @@ const PRIORITY_COLOR: Record<Priority, string> = {
       todo.status === STATUS.finished ? 'opacity-50' : 'opacity-100',
     ]"
   >
-    <template>
-      <div class="mt-6 flex gap-2">
-        <button
-          v-if="todo.status === STATUS.planned"
-          @click="emit('finishTodo', todo.id)"
-          :class="[btn, 'bg-gray-600', 'text-white', 'hover:bg-gray-800']"
-        >
-          Dokončit
-        </button>
+    <div class="mt-6 flex gap-2">
+      <button
+        v-if="todo.status === STATUS.planned"
+        @click="emit('finishTodo', todo.id)"
+        :class="[btn, 'bg-gray-600', 'text-white', 'hover:bg-gray-800']"
+      >
+        Dokončit
+      </button>
 
-        <button
-          @click="emit('deleteTodo', todo.id)"
-          :class="[
-            btn,
-            'border',
-            'border-gray-300',
-            'text-gray-700',
-            'bg-gray-100',
-            'hover:bg-white',
-          ]"
-        >
-          Odstranit
-        </button>
-      </div>
-    </template>
+      <button
+        @click="emit('deleteTodo', todo.id)"
+        :class="[
+          btn,
+          'border',
+          'border-gray-300',
+          'text-gray-700',
+          'bg-gray-100',
+          'hover:bg-white',
+        ]"
+      >
+        Odstranit
+      </button>
+    </div>
   </Card>
 </template>
